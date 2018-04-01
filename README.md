7f1bc7f15a8a82f77bbf75ee06b963b2a4f6fe98
Tal Etinger <etingertal@gmail.com>
2018-04-01 17:43:47 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# zipkin-spring-demo
---
+++ src/test/java/com/sofusit/distributedtracing/DistributedTracingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sofusit.distributedtracing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DistributedTracingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
